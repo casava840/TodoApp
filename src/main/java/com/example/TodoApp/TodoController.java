47fec4f60b90990a33cc19ecab0f177db6501089
“casava840” <hen3403@gmail.com>
2022-10-21 21:14:27 +0900

    dev

---
+++ src/main/java/com/example/TodoApp/TodoController.java
@@ -0,0 +1,12 @@
+package com.example.TodoApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoController {
+    @GetMapping("/")
+    public String helloWorld(){
+        return "To-Do Application !";
+    }
+}
